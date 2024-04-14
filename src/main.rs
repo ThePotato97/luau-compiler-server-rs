9b606ff09adac4d607a5440231fc5b344b7e4c77
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2024-04-14 02:01:12 +0100

    change to json response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 #[macro_use] extern crate rocket;
 use mlua::{Compiler};
+use rocket::serde::{json::Json, Serialize};
+
+#[derive(Serialize)]
+struct Response {
+    code: String,
+}
 
 #[post("/", data = "<code>")]
-fn index(code: String) -> String {
+fn index(code: String) -> Json<Response>  {
     let bytecode = Compiler::new().compile(&code);
     println!("{:?}", code);
 
     let formatted_string = bytecode.iter()
         .map(|byte| format!("\\{}", byte))
         .collect::<String>();
+    let response = Response {
+        code: formatted_string,
+    };
 
-    formatted_string
+    Json(response)
 }
 #[get("/")]
 fn instruct() -> &'static str {
